4cdc8603e9238e54977087e745025a8ec1f8adf7
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2023-05-13 19:17:34 +0200

    Scanf exercices done

---
+++ MaxTwoNums.c
@@ -0,0 +1,13 @@
+#include <stdio.h>/*Directives de preprocessador*/
+
+
+
+/*Programa principal*/
+
+int main()
+{
+
+/*Maxim.c: Programa d'exemple: Calcula el maxim de dos numeros introduits per pantalla
+FUNCIONALITAT
+*/
+}
